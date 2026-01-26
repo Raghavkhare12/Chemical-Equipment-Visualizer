010e44559be112cd2a9f076511939adcd133c335
Raghavkhare12 <raghav122006@gmail.com>
2026-01-26 16:56:24 +0530

    Backend: enable CORS for React integration

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -29,7 +29,6 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -38,12 +37,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    "corsheaders",
     "rest_framework",
     "equipment",
 ]
 
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 
 ROOT_URLCONF = "config.urls"
 
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+CORS_ALLOW_ALL_ORIGINS = True
--- backend/equipment/views.py
+++ backend/equipment/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .models import Dataset
+from .utils import analyze_csv
+
+
+class UploadCSVView(APIView):
+
+    def post(self, request):
+        file = request.FILES.get("file")
+
+        if not file:
+            return Response({"error": "No file uploaded"}, status=400)
+
+        summary = analyze_csv(file)
+
+        Dataset.objects.create(
+            file_name=file.name,
+            summary=summary
+        )
+
+        # Keep only last 5 datasets
+        if Dataset.objects.count() > 5:
+            Dataset.objects.order_by("uploaded_at").first().delete()
+
+        return Response(summary, status=200)
+
+
+class HistoryView(APIView):
+
+    def get(self, request):
+        datasets = Dataset.objects.order_by("-uploaded_at").values()
+        return Response(datasets)
